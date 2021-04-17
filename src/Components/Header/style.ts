52f7dcb06abfac5d5e1a9dd958074a9e02fd115c
Tiago Cambará <tiago.cambara@gmail.com>
2021-04-17 12:25:09 +0100

    feat: add header component

---
+++ src/Components/Header/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+export * from './../../styles/sharedStyles'
+
+export const HeaderContent = styled.header`
+  position: relative;
+  display: block;
+  clear:both;
+`
+
+export const FirstRow = styled.div`
+  display: flex;
+  height: 80px;
+  width: 100%;
+  clear:both;
+`
+
+export const Title = styled.h1`
+  font-family: 'Roboto Slab', serif;
+  line-height: 50%;
+  color: #466F87;
+
+`
+
+export const SecondtRow = styled(FirstRow)`
+  background-color: #659DBD;
+  height: 150px;
+`

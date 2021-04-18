c6d407db0c0cfe4d166c8de17c41a3a99f74e03d
Tiago Cambará <tiago.cambara@gmail.com>
2021-04-18 14:27:55 +0100

    feat: update the container css

--- src/styles/sharedStyles.ts
+++ src/styles/sharedStyles.ts
@@ -2,8 +2,12 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  width: calc(100% - 120px);
-  padding: 30px 10px 40px 20px;
-  margin: 0px 75px;
+  width: calc(100% - 60px);
+  padding: 10px 20px;
+  margin: 0px 30px;
   position: relative;
+  @media(min-width: 768px) {
+    width: calc(100% - 120px);
+    margin: 0px 75px;
+  }
 `

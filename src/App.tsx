45d7ea4cd9d65c7b71bd7c555744f8f41b06a65c
Tiago Cambará <tiago.cambara@gmail.com>
2021-04-18 14:33:44 +0100

    feat: update te app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import Header from './Components/Header';
 import Home from './pages/Home';
-
 import GlobalStyles from './styles/GlobalStyles'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 const App:React.FC = () => {
   return (
     <div>
+      <Header/>
       <Home/>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+      />
       <GlobalStyles/>
     </div>
   );

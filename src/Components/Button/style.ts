8bf0ddfe87426e0aef1816b27fc74bece6ec8505
Tiago Cambará <tiago.cambara@gmail.com>
2021-04-18 14:32:39 +0100

    feat: add button component

---
+++ src/Components/Button/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.div`
+display: inline-flex;
+height: 35px;
+`
+
+export const ButtonContent = styled.button`
+padding: 0.6rem;
+background-color: #0b6192;
+color: #FFF;
+&:hover {
+  background-color: #1c8ed0;
+  cursor: pointer;
+}
+`
